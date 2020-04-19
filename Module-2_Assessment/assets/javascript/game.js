64224c8b3f233e6812eb518af2e3407c47e76f61
Patrick McCorkle <pmccorkle15@alumni.uwosh.edu>
2020-04-18 19:19:47 -0500

    updated js.game

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,17 +1,55 @@
-var Emperors =  {
-"Augustus"
-"Claudius"
-"Vespasian"
-"Nerva"
-"Trajan"
-"Marcus Aurelius"
-"Septimius Severus"
-"Aurelian"
-"Diocletian"
-"Constantine"
-"Julian"
-"Valens"
-"Theodosius"
-"Valentinian"
+//Array of Emperors
+
+const emperors =  [
+
+"Augustus",
+
+"Claudius",
+
+"Vespasian",
+
+"Nerva",
+
+"Trajan",
+
+"Marcus Aurelius",
+
+"Septimius Severus",
+
+"Aurelian",
+
+"Diocletian",
+
+"Constantine",
+
+"Julian",
+
+"Valens",
+
+"Theodosius",
+
+"Valentinian",
+
 "Romulus Augustulus"
+
+];
+
+let answer = ''; 
+
+let MaxMistakes = 5;
+
+let Mistakes = 0;
+
+let guessed = [];
+
+let wordStatus = null;
+
+function randomEmp()  {
+    
+answer = emperors[Math.floor(Math.random() * emperors.length)];
+
+alert(answer);
+
 }
+
+randomEmp();
